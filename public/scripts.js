f5f96b8179483d0dedb7204a459e52c67768e91b
Matthew <matthew.lee.98@gmail.com>
2019-07-14 19:40:45 -0500

    Final Changes

--- public/scripts.js
+++ public/scripts.js
@@ -1,24 +1,25 @@
 var slideIndex = 0;
-showSlides();
+// showSlides();
+document.getElementById("defaultOpen").click();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-    slideIndex = 1
-    }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//     slideIndex = 1
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
 
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
@@ -34,4 +35,3 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
